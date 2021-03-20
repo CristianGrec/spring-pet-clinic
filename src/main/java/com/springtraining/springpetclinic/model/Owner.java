1a437eb619590ac5d12b614073c9e352431ee026
cristian.grec <cristian.grec@softvision.com>
2021-03-20 09:39:38 +0200

    adding POJOs. Closes #1

---
+++ src/main/java/com/springtraining/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springtraining.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springtraining/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springtraining.springpetclinic.model;
+
+public class Vet extends Person{
+}
