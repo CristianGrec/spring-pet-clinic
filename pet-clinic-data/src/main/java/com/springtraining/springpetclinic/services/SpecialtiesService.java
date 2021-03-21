4b19e15de7122e4264041026d9d382e103519593
cristian.grec <cristian.grec@softvision.com>
2021-03-21 23:15:24 +0200

    closes #28

---
+++ pet-clinic-data/src/main/java/com/springtraining/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.springtraining.springpetclinic.services;
+
+import com.springtraining.springpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty,Long>{
+}
